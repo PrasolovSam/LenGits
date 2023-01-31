12f149b2bbeb3c42219f7b30e36eebf039548181
Dmitry <jokkercdv@gmail.com>
2023-01-31 09:49:20 +0300

    swiper slider

--- js/script.js
+++ js/script.js
@@ -1,8 +1,31 @@
-let content = document.querySelector('#name');
+//let leftBtn = document.querySelector("#left");
+//let rightBtn = document.querySelector("#right");
+//let items = document.querySelector("#items");
+//let currentRight = 0;
 
-
-
-
-content.addEventListener('keydown', function(event) {
-
-}
+//rightBtn.addEventListener("click", e => {
+//  e.preventDefault();
+//  currentRight += 350;
+//  items.style.right = `${currentRight}px`;
+//});
+//leftBtn.addEventListener("click", e => {
+//  e.preventDefault();
+//});
+new Swiper('.partner__slider-content', {
+  navigation: {
+    nextEl: '.partner__arrow-right',
+    prevEl: '.partner__arrow-left'
+  },
+  slidesPerView: 3,
+  spaceBetween: 30,
+  loop: true,
+});
+new Swiper('.rewies__wrapp', {
+  navigation: {
+    nextEl: '.rewies__arrow-right',
+    prevEl: '.rewies__arrow-left'
+  },
+  slidesPerView: 3,
+  spaceBetween: 10,
+  loop: true,
+});
