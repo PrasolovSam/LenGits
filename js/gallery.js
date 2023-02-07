bc5498ce6bdc08a4a0077478e8ec47198bb46db6
Dmitry <jokkercdv@gmail.com>
2023-02-07 06:16:47 +0300

    vid

--- js/gallery.js
+++ js/gallery.js
@@ -1,5 +1,6 @@
-let gallBut = document.querySelectorAll ('.gallery__btn');
-let gallBlock = document.querySelectorAll('.wrapp__display');
+let gallBut = document.querySelectorAll ('.gallery__btn-one');
+let gallTwo = document.querySelectorAll ('.gallery__btn-two');
+let gallBlockOne = document.querySelectorAll('.wrapp__display-one');
 let gallMain = document.querySelectorAll('.wrapp__display-main');
 
 
@@ -10,12 +11,30 @@ gallBut.forEach((element, i) => {
   element.addEventListener('click', function () {
     if (!isActive) {
       element.classList.add('g__active');
-      gallBlock[i].classList.add('g__active');
+      gallMain[i].classList.add('g__active');
+      isActive = true;
+    }  else {
+      element.classList.remove ('g__active');
+      gallMain[i].classList.remove ('g__active');
+      isActive = false;
+    }
+
+  });
+});
+
+
+
+gallTwo.forEach((element, i) => {
+  let isActive = false;
+  element.addEventListener('click', function () {
+    if (!isActive) {
+      element.class.add('g__active');
+      gallBlockOne[i].classList.add('g__active');
       isActive = true;
     } else {
       element.classList.remove ('g__active');
-      gallBlock[i].classList.remove ('g__active');
-      isActive = false;
+      gallBlockOne[i].classList.remove ('g__active');
+      isActive = false
     }
   });
 });
