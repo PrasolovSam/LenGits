ee49f50e01d9024c39facbc5842a7d9ab1403acf
Dmitry <jokkercdv@gmail.com>
2023-02-13 12:00:52 +0300

    tab

--- js/gallery.js
+++ js/gallery.js
@@ -1,2 +1,31 @@
-let list = document.querySelectorAll
-let items = document.querySelectorAll
+let listCl = document.querySelectorAll('.gallery__btn');
+let tabsItem = document.querySelectorAll('.wrapp__display');
+
+
+
+listCl.forEach(function (item) {
+  item.addEventListener("click", function () {
+    let currBtn = item;
+    let tabId = currBtn.getAttribute("data-tab");
+    let corrTab = document.querySelector(tabId);
+
+    if (!currBtn.classList.contains('active')) {
+
+
+
+
+      listCl.forEach(function (item) {
+        item.classList.remove('active');
+      });
+
+      tabsItem.forEach(function (item) {
+        item.classList.remove('active');
+      });
+
+
+      currBtn.classList.add('active');
+      corrTab.classList.add('active');
+    }
+  });
+});
+document.querySelector('.gallery__btn').click();
